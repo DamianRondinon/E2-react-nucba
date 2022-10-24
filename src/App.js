883f5d29178293433c050fa2aa0da0f00c1fa2d1
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2022-10-24 08:47:42 -0300

    e2-react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import { BrouserRouter as Router, Switch, Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
